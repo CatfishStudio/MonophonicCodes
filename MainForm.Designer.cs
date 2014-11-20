394005c1479fac6dc26f972b4cfeaa1a3d35fa14
CatfishStudio <somov@i.ua>
2014-11-20 13:19:43 +0300

    Update: 20-11-2014  13:20

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -38,26 +38,34 @@ namespace MonophonicCodes
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
 			this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.правкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
 			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+			this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+			this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
 			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
 			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
 			this.tabControl2 = new System.Windows.Forms.TabControl();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
-			this.tabPage3 = new System.Windows.Forms.TabPage();
-			this.tabPage4 = new System.Windows.Forms.TabPage();
-			this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-			this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
-			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
 			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+			this.tabPage5 = new System.Windows.Forms.TabPage();
+			this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.richTextBox4 = new System.Windows.Forms.RichTextBox();
 			this.menuStrip1.SuspendLayout();
 			this.statusStrip1.SuspendLayout();
 			this.toolStrip1.SuspendLayout();
@@ -69,6 +77,9 @@ namespace MonophonicCodes
 			this.tabPage1.SuspendLayout();
 			this.tabControl2.SuspendLayout();
 			this.tabPage2.SuspendLayout();
+			this.tabPage4.SuspendLayout();
+			this.tabPage5.SuspendLayout();
+			this.tabPage3.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// menuStrip1
@@ -78,34 +89,9 @@ namespace MonophonicCodes
 									this.правкаToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(429, 24);
+			this.menuStrip1.Size = new System.Drawing.Size(743, 24);
 			this.menuStrip1.TabIndex = 0;
 			this.menuStrip1.Text = "menuStrip1";
-			// 
-			// statusStrip1
-			// 
-			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.toolStripStatusLabel1});
-			this.statusStrip1.Location = new System.Drawing.Point(0, 240);
-			this.statusStrip1.Name = "statusStrip1";
-			this.statusStrip1.Size = new System.Drawing.Size(429, 22);
-			this.statusStrip1.TabIndex = 1;
-			this.statusStrip1.Text = "statusStrip1";
-			// 
-			// toolStrip1
-			// 
-			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-									this.toolStripLabel1,
-									this.toolStripTextBox1,
-									this.toolStripSeparator1,
-									this.toolStripButton1,
-									this.toolStripSeparator2,
-									this.toolStripButton2});
-			this.toolStrip1.Location = new System.Drawing.Point(0, 24);
-			this.toolStrip1.Name = "toolStrip1";
-			this.toolStrip1.Size = new System.Drawing.Size(429, 25);
-			this.toolStrip1.TabIndex = 2;
-			this.toolStrip1.Text = "toolStrip1";
 			// 
 			// файлToolStripMenuItem
 			// 
@@ -119,102 +105,40 @@ namespace MonophonicCodes
 			this.правкаToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
 			this.правкаToolStripMenuItem.Text = "Правка";
 			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 324);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(743, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
 			// toolStripStatusLabel1
 			// 
 			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
 			this.toolStripStatusLabel1.Size = new System.Drawing.Size(166, 17);
 			this.toolStripStatusLabel1.Text = "(с) Сомов Евгений Павлович";
 			// 
-			// splitContainer1
+			// toolStrip1
 			// 
-			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer1.Location = new System.Drawing.Point(0, 49);
-			this.splitContainer1.Name = "splitContainer1";
-			// 
-			// splitContainer1.Panel1
-			// 
-			this.splitContainer1.Panel1.Controls.Add(this.tabControl1);
-			// 
-			// splitContainer1.Panel2
-			// 
-			this.splitContainer1.Panel2.Controls.Add(this.tabControl2);
-			this.splitContainer1.Size = new System.Drawing.Size(429, 191);
-			this.splitContainer1.SplitterDistance = 211;
-			this.splitContainer1.TabIndex = 3;
-			// 
-			// tabControl1
-			// 
-			this.tabControl1.Controls.Add(this.tabPage1);
-			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tabControl1.Location = new System.Drawing.Point(0, 0);
-			this.tabControl1.Name = "tabControl1";
-			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(211, 191);
-			this.tabControl1.TabIndex = 0;
-			// 
-			// tabPage1
-			// 
-			this.tabPage1.Controls.Add(this.richTextBox1);
-			this.tabPage1.Location = new System.Drawing.Point(4, 22);
-			this.tabPage1.Name = "tabPage1";
-			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage1.Size = new System.Drawing.Size(203, 165);
-			this.tabPage1.TabIndex = 0;
-			this.tabPage1.Text = "Входные данные";
-			this.tabPage1.UseVisualStyleBackColor = true;
-			// 
-			// richTextBox1
-			// 
-			this.richTextBox1.AcceptsTab = true;
-			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.richTextBox1.Location = new System.Drawing.Point(3, 3);
-			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(197, 159);
-			this.richTextBox1.TabIndex = 0;
-			this.richTextBox1.Text = "";
-			this.richTextBox1.WordWrap = false;
-			// 
-			// tabControl2
-			// 
-			this.tabControl2.Controls.Add(this.tabPage2);
-			this.tabControl2.Controls.Add(this.tabPage3);
-			this.tabControl2.Controls.Add(this.tabPage4);
-			this.tabControl2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tabControl2.Location = new System.Drawing.Point(0, 0);
-			this.tabControl2.Name = "tabControl2";
-			this.tabControl2.SelectedIndex = 0;
-			this.tabControl2.Size = new System.Drawing.Size(214, 191);
-			this.tabControl2.TabIndex = 0;
-			// 
-			// tabPage2
-			// 
-			this.tabPage2.Controls.Add(this.richTextBox2);
-			this.tabPage2.Location = new System.Drawing.Point(4, 22);
-			this.tabPage2.Name = "tabPage2";
-			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(206, 165);
-			this.tabPage2.TabIndex = 0;
-			this.tabPage2.Text = "Результат";
-			this.tabPage2.UseVisualStyleBackColor = true;
-			// 
-			// tabPage3
-			// 
-			this.tabPage3.Location = new System.Drawing.Point(4, 22);
-			this.tabPage3.Name = "tabPage3";
-			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage3.Size = new System.Drawing.Size(206, 165);
-			this.tabPage3.TabIndex = 1;
-			this.tabPage3.Text = "Таблица замен для пропорционального шифра";
-			this.tabPage3.UseVisualStyleBackColor = true;
-			// 
-			// tabPage4
-			// 
-			this.tabPage4.Location = new System.Drawing.Point(4, 22);
-			this.tabPage4.Name = "tabPage4";
-			this.tabPage4.Size = new System.Drawing.Size(206, 165);
-			this.tabPage4.TabIndex = 2;
-			this.tabPage4.Text = "Таблица многоалфавитных подстановок";
-			this.tabPage4.UseVisualStyleBackColor = true;
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.toolStripLabel1,
+									this.toolStripTextBox1,
+									this.toolStripSeparator1,
+									this.toolStripLabel2,
+									this.toolStripComboBox1,
+									this.toolStripSeparator3,
+									this.toolStripButton1,
+									this.toolStripSeparator2,
+									this.toolStripButton2,
+									this.toolStripSeparator4});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(743, 25);
+			this.toolStrip1.TabIndex = 2;
+			this.toolStrip1.Text = "toolStrip1";
 			// 
 			// toolStripLabel1
 			// 
@@ -224,6 +148,7 @@ namespace MonophonicCodes
 			// 
 			// toolStripTextBox1
 			// 
+			this.toolStripTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.toolStripTextBox1.Name = "toolStripTextBox1";
 			this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
 			// 
@@ -231,6 +156,27 @@ namespace MonophonicCodes
 			// 
 			this.toolStripSeparator1.Name = "toolStripSeparator1";
 			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+			// 
+			// toolStripLabel2
+			// 
+			this.toolStripLabel2.Name = "toolStripLabel2";
+			this.toolStripLabel2.Size = new System.Drawing.Size(105, 22);
+			this.toolStripLabel2.Text = "тип шифрования:";
+			// 
+			// toolStripComboBox1
+			// 
+			this.toolStripComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Standard;
+			this.toolStripComboBox1.Items.AddRange(new object[] {
+									"Пропорциональное шифрование",
+									"Многоалфавитные подстановки"});
+			this.toolStripComboBox1.Name = "toolStripComboBox1";
+			this.toolStripComboBox1.Size = new System.Drawing.Size(150, 25);
+			this.toolStripComboBox1.Text = "Пропорциональное шифрование";
+			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
 			// 
 			// toolStripButton1
 			// 
@@ -240,6 +186,7 @@ namespace MonophonicCodes
 			this.toolStripButton1.Name = "toolStripButton1";
 			this.toolStripButton1.Size = new System.Drawing.Size(75, 22);
 			this.toolStripButton1.Text = "Шифровать";
+			this.toolStripButton1.Click += new System.EventHandler(this.ToolStripButton1Click);
 			// 
 			// toolStripSeparator2
 			// 
@@ -255,20 +202,160 @@ namespace MonophonicCodes
 			this.toolStripButton2.Size = new System.Drawing.Size(94, 22);
 			this.toolStripButton2.Text = "Расшифровать";
 			// 
+			// toolStripSeparator4
+			// 
+			this.toolStripSeparator4.Name = "toolStripSeparator4";
+			this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 49);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.tabControl1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.tabControl2);
+			this.splitContainer1.Size = new System.Drawing.Size(743, 275);
+			this.splitContainer1.SplitterDistance = 120;
+			this.splitContainer1.TabIndex = 3;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(743, 120);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.richTextBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(735, 94);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Входные данные";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.AcceptsTab = true;
+			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(729, 88);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// tabControl2
+			// 
+			this.tabControl2.Controls.Add(this.tabPage2);
+			this.tabControl2.Controls.Add(this.tabPage4);
+			this.tabControl2.Controls.Add(this.tabPage5);
+			this.tabControl2.Controls.Add(this.tabPage3);
+			this.tabControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl2.Location = new System.Drawing.Point(0, 0);
+			this.tabControl2.Name = "tabControl2";
+			this.tabControl2.SelectedIndex = 0;
+			this.tabControl2.Size = new System.Drawing.Size(743, 151);
+			this.tabControl2.TabIndex = 0;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.richTextBox2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(735, 125);
+			this.tabPage2.TabIndex = 0;
+			this.tabPage2.Text = "Результат шифрования";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
 			// richTextBox2
 			// 
 			this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.richTextBox2.Location = new System.Drawing.Point(3, 3);
 			this.richTextBox2.Name = "richTextBox2";
-			this.richTextBox2.Size = new System.Drawing.Size(200, 159);
+			this.richTextBox2.Size = new System.Drawing.Size(729, 119);
 			this.richTextBox2.TabIndex = 0;
 			this.richTextBox2.Text = "";
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.Controls.Add(this.richTextBox5);
+			this.tabPage4.Location = new System.Drawing.Point(4, 22);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage4.Size = new System.Drawing.Size(735, 189);
+			this.tabPage4.TabIndex = 1;
+			this.tabPage4.Text = "Результат частотного анализа";
+			this.tabPage4.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox5
+			// 
+			this.richTextBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox5.Location = new System.Drawing.Point(3, 3);
+			this.richTextBox5.Name = "richTextBox5";
+			this.richTextBox5.Size = new System.Drawing.Size(729, 183);
+			this.richTextBox5.TabIndex = 0;
+			this.richTextBox5.Text = "";
+			// 
+			// tabPage5
+			// 
+			this.tabPage5.Controls.Add(this.richTextBox3);
+			this.tabPage5.Location = new System.Drawing.Point(4, 22);
+			this.tabPage5.Name = "tabPage5";
+			this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage5.Size = new System.Drawing.Size(735, 189);
+			this.tabPage5.TabIndex = 2;
+			this.tabPage5.Text = "Таблица пропорционального шифра";
+			this.tabPage5.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox3
+			// 
+			this.richTextBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.richTextBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox3.Location = new System.Drawing.Point(3, 3);
+			this.richTextBox3.Name = "richTextBox3";
+			this.richTextBox3.Size = new System.Drawing.Size(729, 183);
+			this.richTextBox3.TabIndex = 0;
+			this.richTextBox3.Text = "";
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Controls.Add(this.richTextBox4);
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(735, 125);
+			this.tabPage3.TabIndex = 3;
+			this.tabPage3.Text = "Таблица многоалфавитных подстановок";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox4
+			// 
+			this.richTextBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.richTextBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox4.Location = new System.Drawing.Point(3, 3);
+			this.richTextBox4.Name = "richTextBox4";
+			this.richTextBox4.Size = new System.Drawing.Size(729, 119);
+			this.richTextBox4.TabIndex = 1;
+			this.richTextBox4.Text = resources.GetString("richTextBox4.Text");
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(429, 262);
+			this.ClientSize = new System.Drawing.Size(743, 346);
 			this.Controls.Add(this.splitContainer1);
 			this.Controls.Add(this.toolStrip1);
 			this.Controls.Add(this.statusStrip1);
@@ -291,11 +378,22 @@ namespace MonophonicCodes
 			this.tabPage1.ResumeLayout(false);
 			this.tabControl2.ResumeLayout(false);
 			this.tabPage2.ResumeLayout(false);
+			this.tabPage4.ResumeLayout(false);
+			this.tabPage5.ResumeLayout(false);
+			this.tabPage3.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 		}
-		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.RichTextBox richTextBox5;
 		private System.Windows.Forms.TabPage tabPage4;
+		private System.Windows.Forms.RichTextBox richTextBox4;
+		private System.Windows.Forms.RichTextBox richTextBox3;
+		private System.Windows.Forms.TabPage tabPage5;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+		private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+		private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+		private System.Windows.Forms.RichTextBox richTextBox2;
 		private System.Windows.Forms.TabPage tabPage3;
 		private System.Windows.Forms.TabPage tabPage2;
 		private System.Windows.Forms.TabControl tabControl2;
--- MainForm.cs
+++ MainForm.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace MonophonicCodes
 {
@@ -33,6 +34,39 @@ namespace MonophonicCodes
 		void MainFormLoad(object sender, EventArgs e)
 		{
 			
+		}
+		
+		
+		void frequencyAnalysis()
+		{
+			string s = richTextBox1.Text;
+            var x = from c in s
+                    group c by c into g
+                    let count = g.Count()
+                    orderby g.Key ascending
+                    select new
+                    {
+                        Value = g.Key,
+                        Count = count,
+                                          
+                    };
+            richTextBox5.Clear();
+            richTextBox5.Text = "символ: " + "  " + "частота" +" | " + "отн. частота" + "    " +"\n\r";
+            foreach (var count in x)
+            {
+               
+                double a = Convert.ToDouble(count.Count)/s.Length;
+                a=Math.Round(a,5);
+                double b=Math.Round( -1*a*Math.Log(a,2),5);
+                richTextBox5.Text += "  " + count.Value + "                     " + count.Count + "          " +a+"\n\r";
+ 				
+            }
+		}
+		
+		
+		void ToolStripButton1Click(object sender, EventArgs e)
+		{
+			frequencyAnalysis();
 		}
 	}
 }
